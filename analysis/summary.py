266f5815e71a386093b9f82b018981679f663804
Dan Liew <daniel.liew@imperial.ac.uk>
2015-01-05 14:49:23 +0000

    Added simple result type counting script.

---
+++ analysis/summary.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import argparse
+import os
+import logging
+import sys
+import yaml
+
+# HACK
+_file = os.path.abspath(__file__)
+_dir = os.path.dirname(os.path.dirname(_file))
+sys.path.insert(0, _dir)
+from BoogieRunner.ResultType import ResultType
+
+def main(args):
+    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('result_yml', type=argparse.FileType('r'))
+    pargs = parser.parse_args(args)
+
+    results = yaml.load(pargs.result_yml)
+
+    assert isinstance(results, list)
+
+    # Count
+    binned = {}
+    for r in results:
+        resultCode = r['result']
+        try:
+            count = binned[resultCode]
+            binned[resultCode] = count +1
+        except KeyError:
+            binned[resultCode] = 1
+
+    print(binned)
+        
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
