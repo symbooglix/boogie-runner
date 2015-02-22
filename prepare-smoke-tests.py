c8c71fa3148607aca20aaac3c7bfb6fb3a099866
Dan Liew <daniel.liew@imperial.ac.uk>
2015-02-22 22:47:13 +0000

    Make prepare-smoke-test.py concurrent. Running the sbr tool can be quite slow so this speeds things up on the SMACK generated SV-COMP benchmark suite.

--- prepare-smoke-tests.py
+++ prepare-smoke-tests.py
@@ -8,20 +8,31 @@
 """
 import argparse
 import logging
+import multiprocessing
 import os
+import pprint
 from  BoogieRunner import ProgramListLoader
 from BoogieRunner import EntryPointFinder
 import traceback
 import yaml
+import signal
 import sys
 import subprocess
 
 _logger = None
+futureToRunner = None
+
+def handleInterrupt(signum, frame):
+  logging.info('Received signal {}'.format(signum))
+  if futureToRunner != None:
+    cancel(futureToRunner)
 
 def entryPoint(args):
+  global _logger, futureToRunner
   parser = argparse.ArgumentParser(description=__doc__)
   parser.add_argument("-l","--log-level",type=str, default="info", dest="log_level", choices=['debug','info','warning','error'])
   parser.add_argument("--rprefix", default=os.getcwd(), help="Prefix for relative paths for program_list")
+  parser.add_argument("-j", "--jobs", default=multiprocessing.cpu_count(), type=int, help="jobs to run in parallel")
   parser.add_argument("input_program_list", help="File containing list of Boogie programs")
   parser.add_argument("output_dir", help="Directory to create working transformed programs in")
   parser.add_argument("output_program_list")
@@ -35,6 +46,10 @@ def entryPoint(args):
     default=None, help="Get entry point from bool attribute on procedure e.g. {:entry_point}")
 
   pargs = parser.parse_args()
+
+  if pargs.jobs < 1:
+    _logger.error('Jobs must be >= 1')
+    return 1
 
   logLevel = getattr(logging, pargs.log_level.upper(),None)
   if logLevel == logging.DEBUG:
@@ -82,28 +97,80 @@ def entryPoint(args):
 
   # Generate new programs
   _logger.info('Generating new programs')
+  tasks = [ ]
   os.mkdir(pargs.output_dir)
+  for index, (programPath, entryPoint) in enumerate(zip(programList, entryPoints)):
+    outputPath = os.path.join(pargs.output_dir, 'program-{}.bpl'.format(index))
+    tasks.append( ProgramGenTask(programPath, entryPoint, outputPath, pargs.spr_path) )
+
+  # Run
+  if pargs.jobs == 1:
+    for t in tasks:
+      t.run()
+  else:
+    signal.signal(signal.SIGINT, handleInterrupt)
+    signal.signal(signal.SIGTERM, handleInterrupt)
+
+    import concurrent.futures
+    with concurrent.futures.ThreadPoolExecutor(max_workers=pargs.jobs) as executor:
+      futureToRunner = { executor.submit(t.run) : t for t in tasks }
+      for future in concurrent.futures.as_completed(futureToRunner):
+        r = futureToRunner[future]
+        _logger.info('{} runner finished'.format(r.outputPath))
+
+        if future.exception():
+          e = future.exception()
+          cancel(futureToRunner)
+          _logger.error(e)
+          return 1
+
+        if r.exitCode != 0:
+          _logger.error('Tool failed')
+          cancel(futureToRunner)
+          return 1
+
+  # Write program list
   with open(pargs.output_program_list, 'w') as f:
-    for index, (programPath, entryPoint) in enumerate(zip(programList, entryPoints)):
-      _logger.info('{index}: Processing {programPath} with entry point {entryPoint}'.format(index=index, programPath=programPath, entryPoint=entryPoint))
-      outputPath = os.path.join(pargs.output_dir, 'program-{}.bpl'.format(index))
-      f.writelines('# Generated from {}\n'.format(programPath))
-      f.writelines('{}\n\n'.format(outputPath))
-
-      exitCode = subprocess.call([ pargs.spr_path,
-                                   '-e', entryPoint,
-                                   '-p', 'Transform.AxiomAndEntryRequiresCheckTransformPass',
-                                   '-o', outputPath,
-                                   programPath
-                                 ])
-
-      if exitCode != 0:
-        _logger.error('spr failed')
-        return 1
+    for t in tasks:
+      if t.exitCode != None:
+        f.writelines('# Generated from {}\n'.format(t.programPath))
+        f.writelines('{}\n\n'.format(t.outputPath))
+      else:
+        f.writelines('# Skipping program "{}" due to failure\n'.format(t.programPath))
 
   _logger.info('Finished')
 
   return 0
+
+def cancel(futureToRunner):
+  _logger.warning('Cancelling futures')
+  for future in futureToRunner.keys():
+    future.cancel()
+
+class ProgramGenTask:
+  def __init__(self, programPath, entryPoint, outputPath, sprPath):
+    assert isinstance(programPath, str)
+    assert isinstance(entryPoint, str)
+    assert isinstance(outputPath, str)
+    assert isinstance(sprPath, str)
+    assert os.path.exists(programPath)
+    self.programPath = programPath
+    self.entryPoint = entryPoint
+    self.outputPath = outputPath
+    self.sprPath = sprPath
+    self.exitCode = None
+
+  def run(self):
+    cmdLine = [ self.sprPath,
+                '-e', self.entryPoint,
+                '-p', 'Transform.AxiomAndEntryRequiresCheckTransformPass',
+                '-o', self.outputPath,
+                self.programPath
+              ]
+    _logger.debug('Running:\n{}'.format(pprint.pformat(cmdLine)))
+    exitCode = subprocess.call(cmdLine)
+    _logger.debug('Finished')
+    self.exitCode = exitCode
 
 if __name__ == '__main__':
   try:
