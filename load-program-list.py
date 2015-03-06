c5fec34e102cd0bdb537d742ba4e7d6ecd1bffb9
Dan Liew <daniel.liew@imperial.ac.uk>
2015-03-06 16:55:58 +0000

    Added script to check a program list and report how many programs are in it.

---
+++ load-program-list.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# vim: set sw=2 ts=2 softtabstop=2 expandtab:
+"""
+    Script to load and check a program list.
+"""
+import argparse
+import logging
+import os
+from BoogieRunner import ProgramListLoader
+import traceback
+import signal
+import sys
+
+_logger = None
+
+def entryPoint(args):
+  global _logger, futureToRunner
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument("-l","--log-level",type=str, default="info", dest="log_level", choices=['debug','info','warning','error'])
+  parser.add_argument("--rprefix", default=os.getcwd(), help="Prefix for relative paths for program_list")
+  parser.add_argument("program_list", help="File containing list of Boogie programs")
+
+  pargs = parser.parse_args()
+
+  logLevel = getattr(logging, pargs.log_level.upper(),None)
+  if logLevel == logging.DEBUG:
+    logFormat = '%(levelname)s:%(threadName)s: %(filename)s:%(lineno)d %(funcName)s()  : %(message)s'
+  else:
+    logFormat = '%(levelname)s:%(threadName)s: %(message)s'
+
+  logging.basicConfig(level=logLevel, format=logFormat)
+  _logger = logging.getLogger(__name__)
+
+  programList = None
+  try:
+    _logger.debug('Loading program_list from "{}"'.format(pargs.program_list))
+    programList = ProgramListLoader.load(pargs.program_list, pargs.rprefix)
+  except (ProgramListLoader.ProgramListLoaderException) as e:
+    _logger.error(e)
+    _logger.debug(traceback.format_exc())
+    return 1
+
+  logging.info('Loaded {} programs'.format(len(programList)))
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(entryPoint(sys.argv[1:]))
